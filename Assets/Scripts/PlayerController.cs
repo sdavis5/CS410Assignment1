ac5ed4ebc7b60b6d654d984b1ce2b4934c66e126
sdavis5 <spendavi@gmail.com>
2023-04-10 21:49:17 -0700

    Add existing files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private int count;
+    private int numJumps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space) && numJumps < 2)
+        {
+            numJumps++;
+            rb.AddForce(0, 5 * numJumps, 0, ForceMode.Impulse);
+        }
+    }
+
+    void Update()
+    {
+        Vector3 pos = rb.position;
+        if(pos.y <= 0.5 && pos.x < 10 && pos.x > -10 && pos.z < 10 && pos.z > -10)
+        {
+            numJumps = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
+        }
+    }
+}
